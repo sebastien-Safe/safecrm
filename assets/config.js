f9880804ac3fe9385f3e836f6b41f0c7366db39b
sebastien-Safe <sebastien@alonso.biz>
2026-06-11 18:32:35 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -19,5 +19,5 @@
 // du code public (GitHub, site déployé, etc.).
 // =========================================================
 
-const SUPABASE_URL = "https://VOTRE-PROJET.supabase.co";
+const SUPABASE_URL = "https://tqfkdwashuepzstpxdlw.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_ouatARf8Sr2FMfwYXJzEYQ_I0vHaVQO";
